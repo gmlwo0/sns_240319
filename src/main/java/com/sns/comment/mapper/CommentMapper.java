3221002860c3edba7cce4758f9abe5c1f4da52fa
DESKTOP-O7E5F2L\user <test@test.com>
2024-07-22 18:52:00 +0900

    2024-07-22 수업

--- src/main/java/com/sns/comment/mapper/CommentMapper.java
+++ src/main/java/com/sns/comment/mapper/CommentMapper.java
@@ -1,13 +1,23 @@
 package com.sns.comment.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.sns.comment.domain.Comment;
+
 @Mapper
 public interface CommentMapper {
-	
+
 	public int insertComment(
-			@Param("postId") int postId,
-			@Param("userId") int userId,
+			@Param("postId") int postId, 
+			@Param("userId") int userId, 
 			@Param("content") String content);
+	
+	public List<Comment> selectCommentList();
+	
+	public List<Comment> selectCommentListByPostId(int postId);
+	
+	public void deleteCommentById(int id);
 }
