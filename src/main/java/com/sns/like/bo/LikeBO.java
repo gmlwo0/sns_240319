e1ac294eb248cca9c6c23bd401dcac1ee016fd9d
DESKTOP-O7E5F2L\user <test@test.com>
2024-07-24 18:53:18 +0900

    2024-07-24 수업

--- src/main/java/com/sns/like/bo/LikeBO.java
+++ src/main/java/com/sns/like/bo/LikeBO.java
@@ -11,17 +11,43 @@ public class LikeBO {
 	@Autowired
 	private LikeMapper likeMapper;
 	
-	//  input:postId, userId
-	// output:x
+	// input:postId, userId   output:X
 	public void likeToggle(int postId, int userId) {
 		// 조회
-		int count  = likeMapper.selectLikeCountBypostIdUserId(postId, userId);
+		int count = likeMapper.selectLikeCountByPostIdOrUserId(postId, userId);
 		
-		// 삭제 or 추가
+		// 여부 => 삭제 or 추가
 		if (count > 0) {
-			likeMapper.deleteLikeCountBypostIdUserId(userId, postId);
-		}else {
-			likeMapper.insertLikeCountBypostIdUserId(userId, postId);
-		} 
+			// 있으면 삭제
+			likeMapper.deleteLikeByPostIdUserId(postId, userId);
+		} else {
+			// 없으면 추가
+			likeMapper.insertLike(postId, userId);
+		}
+	}
+	
+	// input:postId, userId
+	// output:int
+	public int getLikeCountByPostIdUserId(int postId, int userId) {
+		return likeMapper.selectLikeCountByPostIdOrUserId(postId, userId);
+	}
+	
+	// input: postId
+	// output: int(좋아요 개수)
+	public int getLikeCountByPostId(int postId) {
+		return likeMapper.selectLikeCountByPostIdOrUserId(postId, null);
+	}
+	
+	// 좋아요 채울지 여부
+	// input:postId(필수), userId(로그인/비로그인)
+	// output:boolean (채울지 여부)
+	public boolean filledLikeByPostIdUserId(int postId, Integer userId) {
+		// 비로그인이면 DB 조회 없이 하트 채우지 않음
+		if (userId == null) {
+			return false;
+		}
+		
+		// 로그인이면  1. 행이있으면(1) true   2. 없으면(0) false
+		return likeMapper.selectLikeCountByPostIdOrUserId(postId, userId) == 1 ? true : false;
 	}
 }
--- src/main/java/com/sns/like/mapper/LikeMapper.java
+++ src/main/java/com/sns/like/mapper/LikeMapper.java
@@ -1,15 +1,29 @@
 package com.sns.like.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
-import com.sns.like.domain.Like;
-
-@Mapper 
+@Mapper
 public interface LikeMapper {
 
-	public int selectLikeCountBypostIdUserId(int userId,int postId);
+//	public int selectLikeCountByPostIdUserId(
+//			@Param("postId") int postId,
+//			@Param("userId") int userId);
 	
-	public int deleteLikeCountBypostIdUserId(int userId,int postId);
+//	public int selectLikeCountByPostId(int postId);
 	
-	public int insertLikeCountBypostIdUserId(int userId,int postId);
+	// 카운트 쿼리를 하나로 합친다.
+	public int selectLikeCountByPostIdOrUserId(
+			@Param("postId") int postId,
+			@Param("userId") Integer userId);
+	
+	public void insertLike(
+			@Param("postId") int postId,
+			@Param("userId") int userId);
+	
+	public void deleteLikeByPostIdUserId(
+			@Param("postId") int postId,
+			@Param("userId") int userId);
+	
+	
 }
